fce8b367b09fe60de65de6a89aeae333b5a30362
Dmitry Averyanov <averyanov-22@yandex.ru>
2023-07-31 19:55:19 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/booking/dto/NewBooking.java
+++ src/main/java/ru/practicum/shareit/booking/dto/NewBooking.java
@@ -3,9 +3,7 @@ package ru.practicum.shareit.booking.dto;
 import lombok.Data;
 import ru.practicum.shareit.booking.valid.ValidateStartAndEndBooking;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
